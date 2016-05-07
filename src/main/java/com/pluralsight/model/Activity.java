0fa1589b167e133cd7c61620458bf4b8ca8d0e01
Pritesh <me@pritesh.net>
2016-05-07 20:38:10 +0100

    Commit before moving everything to Spring-Data

---
+++ src/main/java/com/pluralsight/model/Activity.java
@@ -0,0 +1,18 @@
+package com.pluralsight.model;
+
+public class Activity
+{
+
+	private String desc;
+
+	public String getDesc()
+	{
+		return desc;
+	}
+
+	public void setDesc(String desc)
+	{
+		this.desc = desc;
+	}
+
+}
---
+++ src/main/java/com/pluralsight/model/GoalReport.java
@@ -0,0 +1,46 @@
+package com.pluralsight.model;
+
+public class GoalReport
+{
+	private int		goalMinutes;
+	private int		exerciseMinutes;
+	private String	exerciseActivity;
+
+	public GoalReport(int goalMinutes, int exerciseMinutes,
+			String exerciseActivity)
+	{
+		this.goalMinutes = goalMinutes;
+		this.exerciseActivity = exerciseActivity;
+		this.exerciseActivity = exerciseActivity;
+	}
+
+	public final String getExerciseActivity()
+	{
+		return exerciseActivity;
+	}
+
+	public final int getExerciseMinutes()
+	{
+		return exerciseMinutes;
+	}
+
+	public final int getGoalMinutes()
+	{
+		return goalMinutes;
+	}
+
+	public final void setExerciseActivity(String exerciseActivity)
+	{
+		this.exerciseActivity = exerciseActivity;
+	}
+
+	public final void setExerciseMinutes(int exerciseMinutes)
+	{
+		this.exerciseMinutes = exerciseMinutes;
+	}
+
+	public final void setGoalMinutes(int goalMinutes)
+	{
+		this.goalMinutes = goalMinutes;
+	}
+}
